d777f67b1a36784342f75f86e552c4523429a1d2
AssaBaradji <baradjiassa695@gmail.com>
2024-08-22 12:28:42 +0200

    added-files

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+
+import { createApp } from 'vue'
+import './assets/css/style.css'
+
+import { createPinia } from 'pinia';
+import App from './App.vue'
+
+const app = createApp (App)
+
+app.use(createPinia());
+app.mount('#app')
